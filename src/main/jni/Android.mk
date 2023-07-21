d17adce03dea8f87ac6b9339e6f65926c4b2f287
Bang IJAL <119733085+BangJALLL@users.noreply.github.com>
2023-07-21 17:28:40 +0700

    Initial commit

---
+++ src/main/jni/Android.mk
@@ -0,0 +1,41 @@
+LOCAL_PATH := $(call my-dir)
+MAIN_LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libdobby
+LOCAL_SRC_FILES := Dobby/libraries/$(TARGET_ARCH_ABI)/libdobby.a
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_STATIC_LIBRARIES := libdobby
+
+# Here is the name of your lib.
+# When you change the lib name, change also on System.loadLibrary("") under OnCreate method on StaticActivity.java
+# Both must have same name
+LOCAL_MODULE    := MyLibName
+
+# Code optimization
+# -std=c++17 is required to support AIDE app with NDK
+LOCAL_CFLAGS := -Wno-error=format-security -fvisibility=hidden -ffunction-sections -fdata-sections -w
+LOCAL_CFLAGS += -fno-rtti -fno-exceptions -fpermissive
+LOCAL_CPPFLAGS := -Wno-error=format-security -fvisibility=hidden -ffunction-sections -fdata-sections -w -Werror -s -std=c++17
+LOCAL_CPPFLAGS += -Wno-error=c++11-narrowing -fms-extensions -fno-rtti -fno-exceptions -fpermissive
+LOCAL_LDFLAGS += -Wl,--gc-sections,--strip-all, -llog
+LOCAL_ARM_MODE := arm
+
+LOCAL_C_INCLUDES += $(MAIN_LOCAL_PATH)
+
+# Here you add the cpp file
+LOCAL_SRC_FILES := Main.cpp \
+	KittyMemory/KittyMemory.cpp \
+	KittyMemory/MemoryPatch.cpp \
+    KittyMemory/MemoryBackup.cpp \
+    KittyMemory/KittyUtils.cpp \
+    ImGui/imgui.cpp \
+    ImGui/imgui_draw.cpp \
+    ImGui/imgui_widgets.cpp \
+    ImGui/imgui_tables.cpp \
+    ImGui/backends/imgui_impl_opengl3.cpp \
+
+LOCAL_LDLIBS := -llog -landroid -lGLESv2 -lEGL
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ src/main/jni/Includes/ImGuiSetup.h
@@ -0,0 +1,40 @@
+#include <dlfcn.h>
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_internal.h"
+#include "ImGui/backends/imgui_impl_opengl3.h"
+#include "Dobby/dobby.h"
+
+bool setup;
+int glWidth, glHeight;
+
+EGLBoolean (*old_eglSwapBuffers)(EGLDisplay dpy, EGLSurface surface);
+EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
+    eglQuerySurface(dpy, surface, EGL_WIDTH, &glWidth);
+    eglQuerySurface(dpy, surface, EGL_HEIGHT, &glHeight);
+    if (!setup) {
+        ImGui::CreateContext();
+        ImGuiIO &io = ImGui::GetIO();
+        io.DisplaySize = ImVec2((float) glWidth, (float) glHeight);
+        ImGui_ImplOpenGL3_Init();
+        ImFontConfig font_cfg;
+        font_cfg.SizePixels = 22.0f;
+        io.Fonts->AddFontDefault(&font_cfg);
+        setup = true;
+    }
+    ImGuiIO &io = ImGui::GetIO();
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui::NewFrame();
+    DrawESP(glWidth, glHeight);
+    ImGui::EndFrame();
+    ImGui::Render();
+    glViewport(0, 0, (int) io.DisplaySize.x, (int) io.DisplaySize.y);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    return old_eglSwapBuffers(dpy, surface);
+}
+
+void ImGuiHook() {
+    sleep(5);
+    DobbyHook(dlsym(dlopen(OBFUSCATE("libEGL.so"), 4), OBFUSCATE("eglSwapBuffers")), (void *) hook_eglSwapBuffers, (void **) &old_eglSwapBuffers);
+}
